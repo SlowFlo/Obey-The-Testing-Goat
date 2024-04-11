43e49747c7dacd88150f0fd8be104b869f4493e1
slowflo <fbautry@gmail.com>
2024-04-11 14:26:36 +0200

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from lists import views
 from lists.views import home_page
 
 urlpatterns = [
     path("", home_page, name="home"),
+    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
 ]
